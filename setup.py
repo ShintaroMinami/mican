4309f5adbd5b40b490d0b6b2616da107e2ede7f7
ShintaroMinami <sminami@neoleukin.com>
2022-06-26 20:25:55 -0700

    fix version description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 from setuptools.extension import Extension
 from setuptools.command.build_ext import build_ext
 
-#VERSION = 'v1.7.1'
+VERSION='1.9.0'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -19,7 +19,8 @@ class compile_mican(build_ext):
 
 setuptools.setup(
     name="pymican",
-    use_scm_version={'local_scheme': 'no-local-version'},
+    #use_scm_version={'local_scheme': 'no-local-version'},
+    version=VERSION
     author="Shintaro Minami",
     description="Non-sequential protein structure alignment algorithm",
     long_description=long_description,
